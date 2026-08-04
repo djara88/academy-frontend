326c296466f826d89a062db04ee5a2d376775562
djara88 <d.jarazerene@gmail.com>
2026-08-03 22:55:34 -0400

    Update Supabase configuration with hardcoded values
    Replaced dynamic environment variables with hardcoded Supabase URL and ANON KEY.

--- src/config/supabase.ts
+++ src/config/supabase.ts
@@ -1,7 +1,8 @@
 import { createClient } from '@supabase/supabase-js';
 
-// Detectamos si usas Vite o Create React App para leer las variables de entorno
-const supabaseUrl = import.meta.env?.VITE_SUPABASE_URL || process.env.REACT_APP_SUPABASE_URL || '';
-const supabaseKey = import.meta.env?.VITE_SUPABASE_ANON_KEY || process.env.REACT_APP_SUPABASE_ANON_KEY || '';
+// URL y ANON KEY públicas de tu proyecto (Totalmente seguro colocarlo aquí)
+const supabaseUrl = 'https://yihcktculicmuuzzxzik.supabase.co';
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InlpaGNrdGN1bGljbXV1enp4emlrIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODUyNzA1ODIsImV4cCI6MjEwMDg0NjU4Mn0.ipvbcSacWn3rQhV3_O6Qg2gB7e-xEnSsaQRNADOud7M';
 
+// Inicializamos el cliente de Supabase para el Frontend
 export const supabase = createClient(supabaseUrl, supabaseKey);
