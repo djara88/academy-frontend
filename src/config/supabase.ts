d8a7ae1c3a7dc6072ca1adb15763a06155467b04
djara88 <d.jarazerene@gmail.com>
2026-08-03 23:00:24 -0400

    Delete src/config/supabase.ts

--- src/config/supabase.ts
+++
@@ -1,8 +0,0 @@
-import { createClient } from '@supabase/supabase-js';
-
-// URL y ANON KEY públicas de tu proyecto (Totalmente seguro colocarlo aquí)
-const supabaseUrl = 'https://yihcktculicmuuzzxzik.supabase.co';
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InlpaGNrdGN1bGljbXV1enp4emlrIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODUyNzA1ODIsImV4cCI6MjEwMDg0NjU4Mn0.ipvbcSacWn3rQhV3_O6Qg2gB7e-xEnSsaQRNADOud7M';
-
-// Inicializamos el cliente de Supabase para el Frontend
-export const supabase = createClient(supabaseUrl, supabaseKey);
