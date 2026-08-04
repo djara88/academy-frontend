738cb36312b05c1dfba7d5b9438e579d05c24f16
djara88 <d.jarazerene@gmail.com>
2026-08-04 17:18:31 -0400

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route, Navigate, Outlet } from 'react-router-dom';
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 
 // 1. Contexto de Autenticación
