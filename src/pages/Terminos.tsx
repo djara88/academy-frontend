f0abe0e25f8dc12398fd7fb9755a87aa0f89b335
djara88 <d.jarazerene@gmail.com>
2026-08-05 18:22:45 -0400

    Remove unnecessary React import statement

--- src/pages/Terminos.tsx
+++ src/pages/Terminos.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import { useState, useEffect } from 'react';
 import { useAuth } from '../contexts/AuthContext';
 import api from '../api/axiosConfig';
 
